0f5c16e2877b8cd21f2e3efe9976db6826f68243
Mahir5640 <mahirchy929@gmail.com>
2026-03-26 22:37:56 +0200

    Add thermal noise analysis script
    This script generates and analyzes thermal noise, including time domain plotting and power spectral density analysis.

---
+++ thermal_noise_analysis.m
@@ -0,0 +1,30 @@
+% Parameters
+B = 1e6;            % Bandwidth (1 MHz)
+R = 100;            % Resistance (100 Ohms)
+T = 300;            % Temperature (300 Kelvin)
+k = 1.38e-23;       % Boltzmann constant
+n_samples = 10000;  % Number of samples
+
+% Time vector
+time = 0 : 1/B : (n_samples-1) / B;
+
+% Generate Thermal Noise (V_rms = sqrt(4kTRB))
+thermal_noise = sqrt(4 * k * T * R * B) * randn(1, n_samples);
+
+% --- Plotting Time Domain ---
+figure;
+subplot(2,1,1);
+plot(time, thermal_noise);
+title('Thermal Noise - Time Domain');
+xlabel('Time (s)');
+ylabel('Voltage (V)');
+grid on;
+
+% --- Power Spectral Density (PSD) Analysis ---
+[psd, freq] = pwelch(thermal_noise, [], [], [], B);
+subplot(2,1,2);
+semilogx(freq, 10*log10(psd));
+title('Power Spectral Density (PSD)');
+xlabel('Frequency (Hz)');
+ylabel('Power/Frequency (dB/Hz)');
+grid on;
